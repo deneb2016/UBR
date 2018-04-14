ab9a7a3a0f1fbece3c3687527f3f93b6ea429bde
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-04-14 23:35:32 +0900

    adversarial loss

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # hello.py is just for mini-test...
 # You don't have to read this code
-
+#
 # import json
 # anno = json.load(open('/home/seungkwan/data/coco/annotations/instances_train2014.json'))
 #
@@ -57,7 +57,7 @@
 #         c += 1
 #
 # print(c)
-# json.dump(new_anno, open('/home/seungkwan/data/coco/annotations/instances_train2014_subtract_voc.json', 'w'))
+# json.dump(new_anno, open('/home/seungkwan/data/coco/annotations/instances_train2014_subtract_voca.json', 'w'))
 #
 #
 #
@@ -543,43 +543,147 @@ import torch
 #         draw_box(sampled_boxes[j, i, 1:], ['y', 'b'][j])
 # plt.show()
 
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from torch.autograd.function import Function
+# import torch
+# import torch.nn as nn
+# from torch.autograd import Variable
+# from torch.autograd.function import Function
+#
+#
+# class GradientReversalLayer(Function):
+#     @staticmethod
+#     def forward(ctx, input):
+#         print('forward')
+#         return input.clone()
+#
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         print('backward')
+#         return -grad_output
+#
+#
+# a = Variable(torch.FloatTensor([1, 2, 3]))
+# a.requires_grad = True
+#
+# b = Variable(torch.FloatTensor([4, 5, 6]))
+# b.requires_grad = True
+#
+# c = Variable(torch.FloatTensor([2]))
+# c.requires_grad = True
+#
+# ab = a * b
+# ab_r = GradientReversalLayer.apply(ab)
+# ab_r = GradientReversalLayer.apply(ab_r)
+# ab_r = GradientReversalLayer.apply(ab_r)
+#
+# abc = ab_r * c
+# result = abc.sum()
+# print(ab.requires_grad, ab_r.requires_grad)
+# print(a.grad, b.grad, c.grad, ab.grad, abc.grad)
+#
+# result.backward()
+#
+# print(a.grad, b.grad, c.grad, ab.grad, abc.grad)
+
+#
+import json
+anno_train = json.load(open('/home/seungkwan/data/coco/annotations/instances_train2014.json'))
+anno_val = json.load(open('/home/seungkwan/data/coco/annotations/instances_val2014.json'))
+source_category_id = [10,
+                      11,
+                      13,
+                      14,
+                      27,
+                      28,
+                      31,
+                      32,
+                      33,
+                      34,
+                      36,
+                      37,
+                      38,
+                      39,
+                      40,
+                      41,
+                      42,
+                      43,
+                      46,
+                      47,
+                      48,
+                      49,
+                      50,
+                      51,
+                      54,
+                      58,
+                      59,
+                      60,
+                      61,
+                      70,
+                      74,
+                      75,
+                      76,
+                      77,
+                      78,
+                      79,
+                      82,
+                      85,
+                      87,
+                      89]
+including_category = []
+obj_cnt = [0 for i in range(100)]
+for i in range(91):
+    exist = source_category_id.count(i) != 0
+    including_category.append(exist)
+
+print(including_category)
+
+including_image_train = [False for i in range(1000000)]
+including_image_val = [False for i in range(1000000)]
 
 
-class GradientReversalLayer(Function):
-    @staticmethod
-    def forward(ctx, input):
-        print('forward')
-        return input.clone()
+new_anno = dict()
+new_anno['info'] = anno_train['info']
+new_anno['licenses'] = anno_train['licenses']
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        print('backward')
-        return -grad_output
+image_set = []
+object_set = []
+new_categories = []
 
+for ca in anno_train['categories']:
+    if including_category[ca['id']]:
+        new_categories.append(ca)
 
-a = Variable(torch.FloatTensor([1, 2, 3]))
-a.requires_grad = True
+id2idx = {}
+cnt_train_obj = 0
+for obj in anno_train['annotations']:
+    if including_category[obj['category_id']]:
+        including_image_train[obj['image_id']] = True
+        object_set.append(obj)
+        cnt_train_obj += 1
 
-b = Variable(torch.FloatTensor([4, 5, 6]))
-b.requires_grad = True
+cnt_val_obj = 0
+for obj in anno_val['annotations']:
+    if including_category[obj['category_id']]:
+        including_image_val[obj['image_id']] = True
+        object_set.append(obj)
+        cnt_val_obj += 1
 
-c = Variable(torch.FloatTensor([2]))
-c.requires_grad = True
+cnt_train_img = 0
+for img in anno_train['images']:
+    if including_image_train[img['id']]:
+        id2idx[img['id']] = len(image_set)
+        image_set.append(img)
+        cnt_train_img += 1
+cnt_val_img = 0
+for img in anno_val['images']:
+    if including_image_val[img['id']]:
+        id2idx[img['id']] = len(image_set)
+        image_set.append(img)
+        cnt_val_img += 1
 
-ab = a * b
-ab_r = GradientReversalLayer.apply(ab)
-ab_r = GradientReversalLayer.apply(ab_r)
-ab_r = GradientReversalLayer.apply(ab_r)
-
-abc = ab_r * c
-result = abc.sum()
-print(ab.requires_grad, ab_r.requires_grad)
-print(a.grad, b.grad, c.grad, ab.grad, abc.grad)
-
-result.backward()
-
-print(a.grad, b.grad, c.grad, ab.grad, abc.grad)
+print(cnt_train_img, cnt_val_img)
+print(cnt_train_obj, cnt_val_obj)
+new_anno['images'] = image_set
+new_anno['categories'] = new_categories
+new_anno['annotations'] = object_set
+print(len(new_anno['images']), len(new_anno['annotations']))
+json.dump(new_anno, open('/home/seungkwan/data/coco/annotations/instances_trainval2014_40classes.json', 'w'))
