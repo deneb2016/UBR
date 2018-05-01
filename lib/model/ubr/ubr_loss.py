class adversarial loss bug fix, gradient clip for cal...

lib/model/ubr/ubr_loss.py
@@ -338,6 +338,7 @@ class ClassificationAdversarialLoss1(nn.Module):
         self._overlap_threshold = overlap_threshold
         self.classifier = nn.Linear(4096, num_classes)
         self.num_classes = num_classes
+        self.reverse = False
 
     def _init_weights(self):
         def normal_init(m, mean, stddev, truncated=False):
@@ -349,8 +350,15 @@ class ClassificationAdversarialLoss1(nn.Module):
 
         normal_init(self.classifier, 0, 0.001, False)
 
+    def start_reverse_gradient(self):
+        print('start reverse gradient')
+        self.reverse = True
+
     def forward(self, rois, gt_box, shared_feat, gt_labels):
-        shared_feat = GradientReversalLayer.apply(shared_feat)
+        if self.reverse:
+            shared_feat = GradientReversalLayer.apply(shared_feat)
+        else:
+            shared_feat = Variable(shared_feat.data.clone())
         class_pred = self.classifier(shared_feat)
         num_rois = rois.size(0)
         iou = jaccard(rois, gt_box)

lib/model/utils/net_utils.py
@@ -33,19 +33,21 @@ def weights_normal_init(model, dev=0.01):
                 m.weight.data.normal_(0.0, dev)
 
 
-def clip_gradient(model, clip_norm):
+def clip_gradient(models, clip_norm):
     """Computes a gradient clipping coefficient based on gradient norm."""
     totalnorm = 0
-    for p in model.parameters():
-        if p.requires_grad and p.grad is not None:
-            modulenorm = p.grad.data.norm()
-            totalnorm += modulenorm ** 2
+    for model in models:
+        for p in model.parameters():
+            if p.requires_grad and p.grad is not None:
+                modulenorm = p.grad.data.norm()
+                totalnorm += modulenorm ** 2
     totalnorm = np.sqrt(totalnorm)
 
     norm = clip_norm / max(totalnorm, clip_norm)
-    for p in model.parameters():
-        if p.requires_grad and p.grad is not None:
-            p.grad.mul_(norm)
+    for model in models:
+        for p in model.parameters():
+            if p.requires_grad and p.grad is not None:
+                p.grad.mul_(norm)
 
 def vis_detections(im, class_name, dets, thresh=0.8):
     """Visual debugging of detections."""

lib/model/utils/rand_box_generator.py
@@ -24,7 +24,6 @@ class UniformBoxGenerator:
             dy = dy[mask]
             dw = dw[mask]
             dh = dh[mask]
-            print(cnt)
 
             new_cnt = min(seed_pool_size - cnt, mask.sum())
             self._delta[cnt:cnt + new_cnt, 0] = dx[:new_cnt]
@@ -75,10 +74,12 @@ class NaturalBoxGenerator:
             pos_th = 0.35
             scale_th_min = 0.65
             scale_th_max = 1.35
+
             dx = torch.FloatTensor(np.random.uniform(-pos_th, pos_th, seed_pool_size * 10))
             dy = torch.FloatTensor(np.random.uniform(-pos_th, pos_th, seed_pool_size * 10))
             dw = torch.FloatTensor(np.random.uniform(scale_th_min, scale_th_max, seed_pool_size * 10))
             dh = torch.FloatTensor(np.random.uniform(scale_th_min, scale_th_max, seed_pool_size * 10))
+
             boxes = torch.stack([dx, dy, dw, dh], 1)
             iou = jaccard(torch.FloatTensor([[-0.5, -0.5, 0.5, 0.5]]), to_point_form(boxes)).squeeze()
             mask = iou.ge(iou_th)
@@ -86,7 +87,6 @@ class NaturalBoxGenerator:
             dy = dy[mask]
             dw = dw[mask]
             dh = dh[mask]
-            print(cnt)
 
             new_cnt = min(seed_pool_size - cnt, mask.sum())
             self._delta[cnt:cnt + new_cnt, 0] = dx[:new_cnt]
@@ -196,3 +196,63 @@ class UniformIouBoxGenerator:
 
         return ret
 
+
+class NaturalUniformBoxGenerator:
+    def __init__(self, iou_th, seed_pool_size=100000, pos_th=0.35, scale_min=0.5, scale_max=1.5):
+        self._seed_pool_size = seed_pool_size
+        self._delta = torch.zeros((seed_pool_size, 4))
+        self._iou_th = iou_th
+
+        cnt = 0
+        while cnt < seed_pool_size:
+            scale_th_min = np.log(scale_min)
+            scale_th_max = np.log(scale_max)
+            dx = torch.FloatTensor(np.random.uniform(-pos_th, pos_th, seed_pool_size * 10))
+            dy = torch.FloatTensor(np.random.uniform(-pos_th, pos_th, seed_pool_size * 10))
+            dw = torch.FloatTensor(np.exp(np.random.uniform(scale_th_min, scale_th_max, seed_pool_size * 10)))
+            dh = torch.FloatTensor(np.exp(np.random.uniform(scale_th_min, scale_th_max, seed_pool_size * 10)))
+
+            boxes = torch.stack([dx, dy, dw, dh], 1)
+            iou = jaccard(torch.FloatTensor([[-0.5, -0.5, 0.5, 0.5]]), to_point_form(boxes)).squeeze()
+            mask = iou.ge(iou_th)
+            dx = dx[mask]
+            dy = dy[mask]
+            dw = dw[mask]
+            dh = dh[mask]
+
+            new_cnt = min(seed_pool_size - cnt, mask.sum())
+            self._delta[cnt:cnt + new_cnt, 0] = dx[:new_cnt]
+            self._delta[cnt:cnt + new_cnt, 1] = dy[:new_cnt]
+            self._delta[cnt:cnt + new_cnt, 2] = dw[:new_cnt]
+            self._delta[cnt:cnt + new_cnt, 3] = dh[:new_cnt]
+            cnt += new_cnt
+
+        print('init NaturalBoxGenerator')
+
+    def get_rand_boxes(self, base_box, num_gen, im_height, im_width):
+        assert list(base_box.size()) == [4]
+        base_box = to_center_form(base_box.unsqueeze(0)).squeeze()
+
+        selected_indices = torch.LongTensor(np.random.choice(self._seed_pool_size, num_gen * 100, replace=False))
+        dx = self._delta[:, 0][selected_indices]
+        dy = self._delta[:, 1][selected_indices]
+        dw = self._delta[:, 2][selected_indices]
+        dh = self._delta[:, 3][selected_indices]
+
+        ret = torch.zeros(num_gen * 100, 5)
+        ret[:, 1] = base_box[0] + dx * base_box[2]
+        ret[:, 2] = base_box[1] + dy * base_box[3]
+        ret[:, 3] = base_box[2] * dw
+        ret[:, 4] = base_box[3] * dh
+
+        ret[:, 1:] = to_point_form(ret[:, 1:])
+        mask = ret[:, 1].ge(0) * ret[:, 2].ge(0) * ret[:, 3].le(im_width - 1) * ret[:, 4].le(im_height - 1)
+        ratio = dw / dh
+        ratio_mask = ratio.ge(0.333) * ratio.le(3.0)
+        mask *= ratio_mask
+        if mask.sum() == 0:
+            return None
+        ret = ret[mask.unsqueeze(1).expand(ret.size(0), 5)].view(-1, 5)
+
+        ret = ret[:min(num_gen, ret.size(0)), :]
+        return ret